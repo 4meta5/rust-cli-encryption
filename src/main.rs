9678c3fcac32e49ad6e735497d366ef70b5995a7
Glenn Field <glenn@riskroller.com>
2021-05-03 19:52:54 -0500

    Remove tutorial comments

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,8 @@ use std::fs::File;
 use std::io::Write;
 use structopt::StructOpt;
 
-/// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
 struct Cli {
-    /// The path to the file to read
     #[structopt(parse(from_os_str))]
     path: std::path::PathBuf,
     password: String,
